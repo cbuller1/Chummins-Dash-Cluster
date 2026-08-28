681822f3423323c6cf2edea1c9845a9d6034249c
cbuller1 <codybuller325@gmail.com>
2026-08-28 09:53:52 -0600

    Update direct EGLFS dashboard launcher

--- start-dashboard.sh
+++ start-dashboard.sh
@@ -3,6 +3,9 @@
 export QT_QPA_PLATFORM=eglfs
 export QT_QPA_EGLFS_INTEGRATION=eglfs_kms
 
+export QT_QPA_EGLFS_DEBUG=1
+export QT_LOGGING_RULES="qt.qpa.*=true"
+
 PYTHON="/home/chummins/Github/Chummins-Dash-Cluster/.venv-direct/bin/python"
 APP="/home/chummins/Github/Chummins-Dash-Cluster/main.py"
 
