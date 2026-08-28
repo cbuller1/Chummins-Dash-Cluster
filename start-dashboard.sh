23471eb4d56b8e60adf3be53474e5a1c2f339168
Cody Buller <99924592+cbuller1@users.noreply.github.com>
2026-08-28 09:58:00 -0600

    Update start-dashboard.sh

--- start-dashboard.sh
+++ start-dashboard.sh
@@ -3,8 +3,9 @@
 export QT_QPA_PLATFORM=eglfs
 export QT_QPA_EGLFS_INTEGRATION=eglfs_kms
 
-export QT_QPA_EGLFS_DEBUG=1
-export QT_LOGGING_RULES="qt.qpa.*=true"
+# Touchscreen is fixed hardware on this dashboard.
+# Tell Qt exactly which evdev device to use instead of discovering it.
+export QT_QPA_EVDEV_TOUCHSCREEN_PARAMETERS="/dev/input/event4"
 
 PYTHON="/home/chummins/Github/Chummins-Dash-Cluster/.venv-direct/bin/python"
 APP="/home/chummins/Github/Chummins-Dash-Cluster/main.py"
