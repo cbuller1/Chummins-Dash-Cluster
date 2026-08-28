1d6d6e90dffdd198180c1abb19d421d2bee9834e
Cody Buller <99924592+cbuller1@users.noreply.github.com>
2026-08-28 08:13:18 -0600

    Update start-dashboard.sh

--- start-dashboard.sh
+++ start-dashboard.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-SPLASH="/usr/share/plymouth/themes/chummins/boot.png"
 PYTHON="/home/chummins/Github/Chummins-Dash-Cluster/.venv/bin/python"
 APP="/home/chummins/Github/Chummins-Dash-Cluster/main.py"
 
-echo "LAUNCHER: entered at $(cat /proc/uptime)" >&2
-
-/usr/bin/swayimg \
-    -C info.show=no \
-    -f \
-    -s real \
-    "$SPLASH" &
-
-echo "LAUNCHER: swayimg started at $(cat /proc/uptime)" >&2
+echo "LAUNCHER: starting dashboard at $(cat /proc/uptime)" >&2
 
 exec "$PYTHON" "$APP"
